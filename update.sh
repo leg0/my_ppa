1db00f8859938ea10f22f3031467d40db8145ba0
Ahti Legonkov <ahti.legonkov@gmail.com>
2021-12-19 13:59:31 +0200

    bla

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
+EMAIL=leg0@github.com
+
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
